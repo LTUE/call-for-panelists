fb6bf40c43927042646f6a73e5f7bc799a6cab0d
Michael Clark <clark.michael.c@gmail.com>
2019-06-27 16:18:51 -0600

    Removing dead features from registration

--- pages/register.inc.php
+++ pages/register.inc.php
@@ -1,7 +1,7 @@
 <?php defined('INCLUDED') or die(); ?>
 <?php $title = 'Register' ?>
 <?php
-if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['name'])) {
+if (!empty($_POST['email']) && !empty($_POST['password'])) {
     if ($_POST['password'] === $_POST['password-confirm']) {
         $query = $db->prepare('SELECT * FROM accounts WHERE email = :email');
         $query->execute(array(':email' => $_POST['email']));
@@ -12,7 +12,6 @@ if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['name
             $create = $db->prepare('INSERT INTO accounts SET email = :email, password = :password, type = :type');
             $create->execute(array(
                 ':email' => $_POST['email'],
-                ':name' => $_POST['name'],
                 ':type' => 'panelist',
                 ':password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
             ));
@@ -44,12 +43,6 @@ if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['name
 <?php if (!empty($error)): ?>
     <output class="error"><?= $error ?></output>
 <?php endif; ?>
-    <label for="name">What would we know you as?</label>
-    <input type="text" id="name" name="name" required value="<?= $_POST['name'] ?? '' ?>">
-
-    <label for="pen-name">If different, what should we print on your badge, etc? Your pen name.</label>
-    <input type=text" id="pen-name" name="pen-name" value="<?= $_POST['pen-name'] ?? '' ?>">
-
     <label for="email">Login Email:</label>
     <input type="email" id="email" name="email" required value="<?= $_POST['email'] ?? '' ?>">
 
