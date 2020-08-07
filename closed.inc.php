cc42553a2740a38cf497ab061a6b8e83d88c82c7
Iiridayn <iiridayn@gmail.com>
2020-08-06 20:27:03 -0600

    Fix #25 - Matomo configured, also HTTPS redirect

--- closed.inc.php
+++ closed.inc.php
@@ -9,6 +9,23 @@
         body { width: 350px; }
         body.small-form header { width: auto; }
     </style>
+
+    <!-- Matomo -->
+    <script type="text/javascript">
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//stats.ltue.org/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//stats.ltue.org/matomo.php?idsite=2&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
 </head>
 <body class="small-form">
     <header>
--- template.inc.php
+++ template.inc.php
@@ -9,6 +9,24 @@
     <link rel="icon" href="https://ltue.net/wp-content/uploads/2020/01/cropped-AppLogo_512-32x32.png" sizes="32x32" />
     <link rel="icon" href="https://ltue.net/wp-content/uploads/2020/01/cropped-AppLogo_512-192x192.png" sizes="192x192" />
     <link rel="apple-touch-icon-precomposed" href="https://ltue.net/wp-content/uploads/2020/01/cropped-AppLogo_512-180x180.png" />
+
+
+    <!-- Matomo -->
+    <script type="text/javascript">
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//stats.ltue.org/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//stats.ltue.org/matomo.php?idsite=2&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
 </head>
 <body class="large-form">
 <nav>
